2bb936b7b7ffb4b22fb644d65efec159519c60ce
s s <octavalrime@gmail.com>
2022-02-02 11:09:12 +0000

    Add project files.

---
+++ Recursion Test/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace Recursion_Test
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter a number;");
+            int n = Convert.ToInt32(Console.ReadLine());
+            int answer = Factorial(n);
+            Console.WriteLine($"The factorial of your number is {answer}."); // Courtesy of Sofia
+        }
+        static int Factorial(int n)
+        {
+            if (n == 0) { return 1; }
+            else
+            {
+                return n * Factorial(n - 1);
+            }
+        }
+
+    }
+}
